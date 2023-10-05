fd3df7911d16513617ceb4422dd0b79126934463
ncore-x <ncore@internet.ru>
2023-10-06 10:18:14 +1100

    Creating basic templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ivan Ivanov',
+        'title': 'Blog posts 1',
+        'content': 'First post content',
+        'date_posted': 'October 5, 2023',
+    },
+    {
+        'author': 'Petr Petrov',
+        'title': 'Blog posts 2',
+        'content': 'Second post content',
+        'date_posted': 'September 10, 2023',
+    },
+]
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
